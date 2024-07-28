4f83e6333726f1e124f0984e543583d290b869ba
abhay8696 <kambleabhaygautam@gmail.com>
2024-07-28 21:08:24 +0530

    onstalled tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,8 @@ function App() {
   return (
     <>
       <div>
-        <h1>Entertainment Web App</h1>
+        <h1 className='text-xl font-bold text-red-500'>Entertainment Web App</h1>
+        <Navbar />
       </div>
     </>
   )
