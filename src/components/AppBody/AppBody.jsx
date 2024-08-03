10826c02fbfa38f3c0fca076cb4a50b3ec670e2d
abhay8696 <kambleabhaygautam@gmail.com>
2024-08-03 21:37:52 +0530

    gave fixed postion to navbar and gave extra padding to AppBody to compansate fixed position of navabar

--- src/components/AppBody/AppBody.jsx
+++ src/components/AppBody/AppBody.jsx
@@ -8,7 +8,7 @@ import Recommendations from '../Recommendations/Recommendations';
 
 const AppBody = () => {
     return (
-        <div className='AppBody text-start flex flex-col gap-6 md:gap-8 my-6 md:my-8'>
+        <div className='AppBody text-start flex flex-col gap-6 md:gap-8 pt-14 lg:pt-0 lg:pl-24 my-6 md:my-8'>
             <div className='px-4 md:px-0 lg:px-9'>
                 <SearchComp />
             </div>
